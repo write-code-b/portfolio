e1efcac6163d68a6dd813e226fc1644876a7d2a1
write-code-b <minjichoi.official@gmail.com>
2024-06-18 10:41:21 +0900

    update tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,14 +2,44 @@ import { useEffect, useState } from "react";
 
 function Tech(props) {
   return (
-    <section id="tect_section">
-      <h1>기술입니다.</h1>
-      <p>
-        HTML, CSS, Javascript를 활용한 퍼블리싱을 합니다. 전자교과서 관련
-        퍼블리싱 외주 프로젝트 경험이 있습니다. 앞으로도 교육, 출판 분야에서의
-        다양한 퍼블리싱 프로젝트를 진행할 계획입니다. 재택 업무를 희망하고,
-        오프라인 미팅은 가능합니다. 감사합니다.
-      </p>
+    <section id="tech_section">
+      <div>
+        <h2>프론트엔드</h2>
+        <p>
+          <div className="badge">HTML</div>
+          <div className="badge">CSS</div>
+          <div className="badge">Javascript</div>
+        </p>
+      </div>
+      <div>
+        <h2>백엔드</h2>
+        <p>
+          <div className="badge">Node.js</div>
+          <div className="badge">Javascript</div>
+        </p>
+      </div>
+      <div>
+        <h2>디자인</h2>
+        <p>
+          <div className="badge">Figma</div>
+          <div className="badge">Adobe Illustrator</div>
+        </p>
+      </div>
+      <div>
+        <h2>형상관리</h2>
+        <p>
+          <div className="badge">GIT</div>
+          <div className="badge">SVN</div>
+        </p>
+      </div>
+      <div>
+        <h2>그 외 기술</h2>
+        <p>
+          <div className="badge">빅데이터분석기사</div>
+          <div className="badge">python</div>
+          <div className="badge">MySQL</div>
+        </p>
+      </div>
     </section>
   );
 }
