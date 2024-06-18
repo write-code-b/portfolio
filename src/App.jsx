fc92d46db18fc8d6bf99687c76a011c0e28a1e7d
write-code-b <minjichoi.official@gmail.com>
2024-06-18 10:21:46 +0900

    add default active tab in introduce page

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Footer from "./components/Footer";
 import Content from "./components/Content";
 
 const DATA = [
-  { number: "0", name: "자기소개"},
-  { number: "1", name: "기술" },
-  { number: "2", name: "연락처" },
+  { number: "0", name: "자기소개", isPressed: true },
+  { number: "1", name: "기술", isPressed: false },
+  { number: "2", name: "연락처", isPressed: false },
 ];
 
 function App(props) {
