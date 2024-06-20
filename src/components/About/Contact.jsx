a29cf25edcc500c4c7ee67eb98560f7fce84c29f
write-code-b <minjichoi.official@gmail.com>
2024-06-20 10:04:46 +0900

    update contact section in about page

--- src/components/About/Contact.jsx
+++ src/components/About/Contact.jsx
@@ -1,20 +1,17 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 
 function Contact(props) {
-  const copyToClipboard = async (tooltipRef, e) => {
+  const copyToClipboard = async (e) => {
     try {
-      const link = e.target.parentNode.innerText;
+      const target = e.target;
+      const link = target.parentNode.firstChild.innerText;
       console.log(`[Success] copy to clipboard / link: ${link}`);
       await navigator.clipboard.writeText(link);
-      tooltipRef.current.innerText = "완료";
+      target.firstChild.innerText = "완료";
     } catch (err) {
       console.log("[contact.jsx] fail copy to clipboard");
     }
   };
-
-  const tooltipRef1 = useRef(null);
-  const tooltipRef2 = useRef(null);
-  const tooltipRef3 = useRef(null);
 
   return (
     <section id="contact_section">
@@ -24,15 +21,12 @@ function Contact(props) {
         <div className="wrap">
           <div className="badge">
             minjichoi.official@gmail.com
-            <div className="tooltip_text">메일로 문의주시면 가능한 빨리 회신드리겠습니다.</div>
-          </div>
-          <div
-            className="copy_icon"
-            onClick={(e) => copyToClipboard(tooltipRef1, e)}
-          >
-            <div className="tooltip_text" ref={tooltipRef1}>
-              복사
+            <div className="tooltip_text">
+              메일로 문의주시면 가능한 빨리 회신드리겠습니다.
             </div>
+          </div>
+          <div className="copy_icon" onClick={copyToClipboard}>
+            <div className="tooltip_text">복사</div>
           </div>
         </div>
       </div>
@@ -40,13 +34,8 @@ function Contact(props) {
         <h2>깃헙</h2>
         <div className="wrap">
           <div className="badge">https://github.com/slow-wave</div>
-          <div
-            className="copy_icon"
-            onClick={(e) => copyToClipboard(tooltipRef2, e)}
-          >
-            <div className="tooltip_text" ref={tooltipRef2}>
-              복사
-            </div>
+          <div className="copy_icon" onClick={copyToClipboard}>
+            <div className="tooltip_text">복사</div>
           </div>
         </div>
         <div className="wrap">
@@ -54,13 +43,8 @@ function Contact(props) {
             https://github.com/write-code-b
             <div className="tooltip_text">퍼블리싱 관련 코드 저장소입니다.</div>
           </div>
-          <div
-            className="copy_icon"
-            onClick={(e) => copyToClipboard(tooltipRef3, e)}
-          >
-            <div className="tooltip_text" ref={tooltipRef3}>
-              복사
-            </div>
+          <div className="copy_icon" onClick={copyToClipboard}>
+            <div className="tooltip_text">복사</div>
           </div>
         </div>
       </div>
