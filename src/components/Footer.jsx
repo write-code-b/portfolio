86674d8613a6374209290493a95c206ed8a23371
write-code-b <minjichoi.official@gmail.com>
2024-06-20 09:52:17 +0900

    update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,18 @@ function Footer(props) {
     <footer>
       <div>
         <p>Copyright © 2024</p>
-        <p>최민지</p>
         <p>
-          <a href="mailto:minjichoi.write@kakao.com">minjichoi.write@kakao.com</a>
+          <a
+            href="https://www.figma.com/design/JqccT1mpAdMfikXp5Cx3hb/MINJI-CHOI-portfolio?node-id=303-2&t=od7urjG4Vpuy3b0B-1"
+            target="_blank"
+          >
+            Designed
+          </a>
+          and
+          <a href="https://github.com/write-code-b/portfolio" target="_blank">
+            Developed
+          </a>
+          by 최민지
         </p>
       </div>
     </footer>
